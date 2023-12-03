ed684d0cdd2ccead659f551165aedbd8e870b729
kepler <kepler3141@gmail.com>
2023-12-03 23:54:15 +0900

    FEAT: Change signup

--- common/forms.py
+++ common/forms.py
@@ -2,11 +2,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
-
 class UserForm(UserCreationForm):
     password2 = None
-    phone_number = forms.CharField(max_length=15, required=False, help_text='Optional.')
 
     class Meta:
         model = User
-        fields = UserCreationForm.Meta.fields + ("username", "password1", 'phone_number')
+        fields = ("username", "password1")
