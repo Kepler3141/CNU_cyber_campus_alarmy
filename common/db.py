20cdd35405f74f6eb6ebccd78f16701c3572a6e2
kepler <kepler3141@gmail.com>
2023-12-12 04:18:54 +0900

    FEAT: Test

---
+++ common/db.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+dataBase = pd.DataFrame({'studentNumber': ['admin'],
+                         'password': ['1111'],
+                         'email': ['admin@admin.com'],
+                         'data': ['']})
