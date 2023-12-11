28f2862e3d1c34b900bfc1f329e5a72989aa0049
kepler <kepler3141@gmail.com>
2023-12-12 04:14:30 +0900

    Apply .gitignore

--- common/db.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-dataBase = pd.DataFrame({'studentNumber': ['admin'],
-                         'password': ['1111'],
-                         'email': ['admin@admin.com'],
-                         'data': ['']})
