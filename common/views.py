2ff3ee5d769f9230eed2cf6fb367e31eebbf0a4b
kepler <kepler3141@gmail.com>
2023-12-17 19:42:37 +0900

    FEAT: Test

--- common/views.py
+++ common/views.py
@@ -9,15 +9,15 @@ import pandas as pd
 def signup(request):
     if request.method == "POST":
         form = UserForm(request.POST)
-        print(request.POST.get("username"))
-        print(request.POST.get("password1"))
-        print(request.POST.get("email"))
-        new_data = pd.DataFrame({'studentNumber': request.POST.get("username"),
-                                 'password': request.POST.get("password1"),
-                                 'email': request.POST.get("email"),
-                                 'data': ['']})
-        common.db.dataBase = pd.concat([common.db.dataBase, new_data])
-        print(common.db.dataBase)
+        # print(request.POST.get("username"))
+        # print(request.POST.get("password1"))
+        # print(request.POST.get("email"))
+        # new_data = pd.DataFrame({'studentNumber': request.POST.get("username"),
+        #                          'password': request.POST.get("password1"),
+        #                          'email': request.POST.get("email"),
+        #                          'data': ['']})
+        # common.db.dataBase = pd.concat([common.db.dataBase, new_data])
+        # print(common.db.dataBase)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
