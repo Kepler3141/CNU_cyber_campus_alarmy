d9e11abcd2101183788e8a72f60521c39e359d2a
kepler <kepler3141@gmail.com>
2023-12-17 15:19:39 +0900

    FEAT: Test

--- common/db.py
+++ common/db.py
@@ -3,4 +3,4 @@ import pandas as pd
 dataBase = pd.DataFrame({'studentNumber': ['admin'],
                          'password': ['1111'],
                          'email': ['admin@admin.com'],
-                         'data': ['']})
+                         'data': ['null']})
